c7676114b4d764486516611d2cb7b58482c36810
zwoong <zwoong6867@gmail.com>
2025-01-21 00:10:44 +0900

    ✨ Check 컴포넌트 수정

--- src/common/components/Check/index.jsx
+++ src/common/components/Check/index.jsx
@@ -1,13 +1,19 @@
 import style from "./index.module.css";
 
-function Check ({content, id}) {
+function Check({ content, id, onChange }) {
   return (
     <label htmlFor={id} className={style.check_form}>
-      <input type="checkbox" name="check" id={id} className={style.true_check} />
+      <input
+        type="checkbox"
+        name="check"
+        id={id}
+        className={style.true_check}
+        onChange={onChange} // onChange 이벤트 핸들러 추가
+      />
       <div className={style.fake_check}></div>
       <p>{content}</p>
-    </label>  
-  )
+    </label>
+  );
 }
 
 export default Check;
